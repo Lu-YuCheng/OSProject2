3ccc47bfd8f971705d3fb3c0f6c4fb311befe3b2
b05902132 <b05902132@csie.ntu.edu.tw>
2019-06-09 10:39:44 +0800

    Fix slave mmapping

--- master_device/master_device.c
+++ master_device/master_device.c
@@ -29,7 +29,7 @@
 #define master_IOCTL_CREATESOCK 0x12345677
 #define master_IOCTL_MMAP 0x12345678
 #define master_IOCTL_EXIT 0x12345679
-#define BUF_SIZE 512
+#define BUF_SIZE 4096
 #define NPAGES 50
 
 typedef struct socket *ksocket_t;
